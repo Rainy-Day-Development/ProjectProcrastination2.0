30c2f2b42c71ffa95127fd3fae8aa69ca621c921
Rainy-Day-Development <35881097+Rainy-Day-Development@users.noreply.github.com>
2018-02-03 18:24:28 -0800

    Kris
    Committing base project

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+    public bool Current = false;
+    public bool Target = false;
+    public bool Selectable = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Current)
+        {
+            GetComponent<Renderer>().material.color = Color.magenta;
+        }
+        else if (Target)
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+        else if (Selectable)
+        {
+            GetComponent<Renderer>().material.color = Color.cyan;
+        }
+        else {
+            GetComponent<Renderer>().material.color = Color.white;
+        }
+
+
+
+
+	}
+}
